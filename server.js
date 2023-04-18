a75549f08a6ef080d8e145bb07d2df40c3547998
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2023-04-18 23:21:38 +0530

    modified server js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("*", cors());
 
 app.get("/", (req, res) => {
-	res.send("User API is Running");
+	res.send("API is Running");
 });
 
 app.use("/user", proxy("http://localhost:5002"));
